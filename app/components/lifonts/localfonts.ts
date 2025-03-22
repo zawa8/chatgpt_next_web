091591bea9402d616b97d4bd040aa8e63412d98e
viml kumar <vimalkg@gmail.com>
2025-03-22 15:50:21 +0530

    mck82.vercel.app zikis8.vercel.app fb.com/ztrabc

--- app/components/lifonts/localfonts.ts
+++ app/components/lifonts/localfonts.ts
@@ -12,27 +12,15 @@ export const ing15 = localFont({
   src: "./fonts/15_woff2/ing15.woff2",
   display: "swap",
 });
-export const iing15 = localFont({
-  src: "./fonts/15_woff2/iing15.woff2",
-  display: "swap",
-});
-export const ing25 = localFont({
-  src: "./fonts/25_woff2/ing25.woff2",
-  display: "swap",
-});
-export const ing4 = localFont({
-  src: "./fonts/4_woff2/ing4.woff2",
-  display: "swap",
-});
+// export const iing15 = localFont({ src: "./fonts/15_woff2/iing15.woff2", display: "swap", });
+// export const ing25 = localFont({ src: "./fonts/25_woff2/ing25.woff2", display: "swap", });
+// export const ing4 = localFont({ src: "./fonts/4_woff2/ing4.woff2", display: "swap", });
 /////////////////
 export const hindi15 = localFont({
   src: "./fonts/15_woff2/hindi15.woff2",
   display: "swap",
 });
-export const ihindi15 = localFont({
-  src: "./fonts/15_woff2/ihindi15.woff2",
-  display: "swap",
-});
+// export const ihindi15 = localFont({ src: "./fonts/15_woff2/ihindi15.woff2", display: "swap", });
 export const bangla15 = localFont({
   src: "./fonts/15_woff2/bangla15.woff2",
   display: "swap",
@@ -74,50 +62,17 @@ export const sinhala15 = localFont({
   display: "swap",
 });
 /////////////////
-export const hindi25 = localFont({
-  src: "./fonts/25_woff2/hindi25.woff2",
-  display: "swap",
-});
-export const bangla25 = localFont({
-  src: "./fonts/25_woff2/bangla25.woff2",
-  display: "swap",
-});
-export const gurmukhi25 = localFont({
-  src: "./fonts/25_woff2/gurmukhi25.woff2",
-  display: "swap",
-});
-export const oriya25 = localFont({
-  src: "./fonts/25_woff2/oriya25.woff2",
-  display: "swap",
-});
-export const korean25 = localFont({
-  src: "./fonts/25_woff2/korean25.woff2",
-  display: "swap",
-});
-export const guzrati25 = localFont({
-  src: "./fonts/25_woff2/guzrati25.woff2",
-  display: "swap",
-});
-export const tamil25 = localFont({
-  src: "./fonts/25_woff2/tamil25.woff2",
-  display: "swap",
-});
-export const telugu25 = localFont({
-  src: "./fonts/25_woff2/telugu25.woff2",
-  display: "swap",
-});
-export const kannada25 = localFont({
-  src: "./fonts/25_woff2/kannada25.woff2",
-  display: "swap",
-});
-export const malayalam25 = localFont({
-  src: "./fonts/25_woff2/malayalam25.woff2",
-  display: "swap",
-});
-export const sinhala25 = localFont({
-  src: "./fonts/25_woff2/sinhala25.woff2",
-  display: "swap",
-});
+// export const hindi25 = localFont({ src: "./fonts/25_woff2/hindi25.woff2", display: "swap", });
+// export const bangla25 = localFont({ src: "./fonts/25_woff2/bangla25.woff2", display: "swap", });
+// export const gurmukhi25 = localFont({ src: "./fonts/25_woff2/gurmukhi25.woff2", display: "swap", });
+// export const oriya25 = localFont({ src: "./fonts/25_woff2/oriya25.woff2", display: "swap", });
+// export const korean25 = localFont({ src: "./fonts/25_woff2/korean25.woff2", display: "swap", });
+// export const guzrati25 = localFont({ src: "./fonts/25_woff2/guzrati25.woff2", display: "swap", });
+// export const tamil25 = localFont({ src: "./fonts/25_woff2/tamil25.woff2", display: "swap", });
+// export const telugu25 = localFont({ src: "./fonts/25_woff2/telugu25.woff2", display: "swap", });
+// export const kannada25 = localFont({ src: "./fonts/25_woff2/kannada25.woff2", display: "swap", });
+// export const malayalam25 = localFont({ src: "./fonts/25_woff2/malayalam25.woff2", display: "swap", });
+// export const sinhala25 = localFont({ src: "./fonts/25_woff2/sinhala25.woff2", display: "swap", });
 ///////////////////
 /*//
 bangla15    guzrati15  kannada15  malayalam15  sinhala15  telugu15
